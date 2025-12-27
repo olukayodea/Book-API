955a558e394e70a792ef675405f6b23c3e2ba2a2
Olukayode Adebiyi <olukayode.adebiyi@hotmail.co.uk>
2025-12-27 13:34:26 +0100

    update
    Add CORS policy to enable cross-origin requests
    
    Added a CORS policy named "MyCors" to allow any origin,
    header, and method. Registered the policy in the service
    container using `AddCors` and applied it in the middleware
    pipeline with `app.UseCors("MyCors")`. This change enables
    the application to handle cross-origin requests, facilitating
    frontend-backend communication across different domains.

--- Book API/Program.cs
+++ Book API/Program.cs
@@ -1,9 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 // add services to the container
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCors", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 app.MapControllers();
+app.UseCors("MyCors");
 
 app.MapGet("/", () =>
 {
